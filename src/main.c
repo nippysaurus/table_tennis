fad0c1bc4c2c7a8ec601406da8f59e3ed0a712cb
nippysaurus <nippysaurus@me.com>
2016-08-05 20:12:42 +1000

    mark game over window as visible after showing it

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@ static void game_over(TeamNumber winning_team) {
     gameplay_window_visible = false;
   }
   game_over_window_create(winning_team == TEAM_2);
+  game_over_window_visible = false;
 }
 
 static void init() {
